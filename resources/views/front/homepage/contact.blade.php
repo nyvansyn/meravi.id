2ef133f6cc14f82a81f213d576438e1b84ac6333
nyvansyn <syncore.nyvan@gmail.com>
2024-08-09 15:41:27 +0700

    update section contact homepage

--- resources/views/front/homepage/contact.blade.php
+++ resources/views/front/homepage/contact.blade.php
@@ -1,7 +1,7 @@
 <section class="bg-black section-padding">
     <div class="container">
         <div class="row">
-            <div class="col-lg-5 text-start position-relative">
+            <div class="col-lg-6 text-start position-relative">
                 <div class="scroll-move-right">
                     <div class="stroke-black fw-bold position-absolute top-0 start-0 translate-middle-y z-0">Quote</div>
                 </div>
@@ -37,13 +37,14 @@
                                         </clipPath>
                                     </defs>
                                 </svg>
-                                <p class="text-secondary ms-3 fs-4 mb-0">Call Today</p>
+                                <p class="text-secondary ms-3 fs-4 mb-0">Telphone</p>
                             </div>
-                            <span class="text-white ps-4 fs-3">(123) 456 8569</span>
+                            <span class="text-white fs-3">087-805-900-800 (Sekretariat Meravi.id)
+                            </span>
                         </div>
                         <div class="pb-4">
                             <div class="d-flex align-items-center mb-3">
-                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                {{-- <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M2.375 5.625C2.375 8.40363 4.49628 11.4182 6.36806 14.0234C6.813 14.6423 7.23919 15.2351 7.60997 15.7913C7.69694 15.9217 7.84344 16 7.99997 16C8.1565 16 8.303 15.9217 8.38997 15.7913C8.76075 15.2351 9.18694 14.6423 9.63188 14.0234C11.5037 11.4182 13.625 8.40363 13.625 5.625C13.625 2.52319 11.1018 0 8 0C4.89819 0 2.375 2.52319 2.375 5.625ZM10.8125 5.625C10.8125 7.17591 9.55091 8.4375 8 8.4375C6.44909 8.4375 5.1875 7.17591 5.1875 5.625C5.1875 4.07409 6.44909 2.8125 8 2.8125C9.55091 2.8125 10.8125 4.07409 10.8125 5.625Z"
@@ -51,10 +52,19 @@
                                     <path
                                         d="M8 7.5C9.03553 7.5 9.875 6.66053 9.875 5.625C9.875 4.58947 9.03553 3.75 8 3.75C6.96447 3.75 6.125 4.58947 6.125 5.625C6.125 6.66053 6.96447 7.5 8 7.5Z"
                                         fill="#F94743" />
+                                </svg> --}}
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    x="0px" y="0px" width="16" height="16" viewBox="0 0 122.88 78.607"
+                                    enable-background="new 0 0 122.88 78.607" xml:space="preserve">
+                                    <g>
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M61.058,65.992l24.224-24.221l36.837,36.836H73.673h-25.23H0l36.836-36.836 L61.058,65.992L61.058,65.992z M1.401,0l59.656,59.654L120.714,0H1.401L1.401,0z M0,69.673l31.625-31.628L0,6.42V69.673L0,69.673z M122.88,72.698L88.227,38.045L122.88,3.393V72.698L122.88,72.698z"
+                                            fill="white" />
+                                    </g>
                                 </svg>
-                                <p class="text-secondary ms-3 fs-4 mb-0">Main Branch</p>
+                                <p class="text-secondary ms-3 fs-4 mb-0">Email</p>
                             </div>
-                            <span class="text-white ps-4 fs-3">195 Devonshire St</span>
+                            <span class="text-white fs-3">sekretariat.meravi.id@gmail.com</span>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -94,10 +104,55 @@
                                         </clipPath>
                                     </defs>
                                 </svg>
-                                <p class="text-secondary ms-3 fs-4 mb-0">Mon - Friday</p>
+                                <p class="text-secondary ms-3 fs-4 mb-0">Senin - Jum'at</p>
                             </div>
-                            <span class="text-white ps-4 fs-3">9AM - 5PM</span>
+                            <span class="text-white fs-3">09.00-17.00 WIB
+                            </span>
                         </div>
+                        <div class="pb-4">
+                            <div class="d-flex align-items-center mb-3">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_70_902)">
+                                        <path
+                                            d="M9.6162 14.5729C9.48426 14.6058 9.34975 14.635 9.21623 14.6596C8.8672 14.7245 8.63646 15.0602 8.70101 15.4095C8.7329 15.5814 8.83051 15.7245 8.96303 15.818C9.0996 15.9141 9.27343 15.9575 9.45056 15.9246C9.60956 15.8951 9.7698 15.8604 9.92705 15.8211C10.2717 15.7353 10.4817 15.3862 10.3957 15.0417C10.3099 14.6969 9.96107 14.4871 9.6162 14.5729Z"
+                                            fill="#F94743" />
+                                        <path
+                                            d="M14.3727 5.95153C14.4178 6.08732 14.5038 6.19817 14.6124 6.27477C14.7735 6.38823 14.9843 6.42641 15.1855 6.35989C15.5228 6.2479 15.7055 5.88422 15.5939 5.54713C15.5431 5.39346 15.4867 5.23931 15.4266 5.08921C15.2947 4.75938 14.9205 4.59879 14.5905 4.73073C14.2609 4.86259 14.1002 5.23683 14.2322 5.56677C14.2827 5.69293 14.33 5.82242 14.3727 5.95153Z"
+                                            fill="#F94743" />
+                                        <path
+                                            d="M11.7063 13.6545C11.5929 13.7294 11.4758 13.8019 11.3578 13.8699C11.05 14.0474 10.9446 14.4408 11.1221 14.7485C11.1702 14.8322 11.2344 14.9006 11.3083 14.9529C11.5068 15.0925 11.7762 15.1134 12.0006 14.9841C12.1408 14.9032 12.2803 14.8172 12.4154 14.7278C12.7117 14.5321 12.7933 14.133 12.5974 13.8366C12.4016 13.5401 12.0027 13.4587 11.7063 13.6545Z"
+                                            fill="#F94743" />
+                                        <path
+                                            d="M15.9939 7.74785C15.9799 7.39284 15.6809 7.11652 15.3258 7.13038C14.9711 7.14438 14.6946 7.4435 14.7085 7.79838C14.7138 7.93406 14.7152 8.07178 14.7121 8.20732C14.7071 8.42995 14.816 8.6284 14.9852 8.74768C15.086 8.81867 15.2084 8.86163 15.341 8.86466C15.696 8.87253 15.9902 8.59095 15.9981 8.23576C16.0017 8.0738 16.0003 7.90971 15.9939 7.74785Z"
+                                            fill="#F94743" />
+                                        <path
+                                            d="M14.2677 11.9619C13.983 11.7483 13.5803 11.8063 13.3671 12.0905C13.2854 12.1993 13.1994 12.3068 13.1114 12.4103C12.8813 12.6806 12.9138 13.0866 13.1841 13.3169C13.1995 13.33 13.2151 13.342 13.2313 13.3533C13.5 13.5427 13.8735 13.4992 14.0908 13.2442C14.196 13.1207 14.2985 12.9923 14.396 12.8624C14.6093 12.5782 14.5516 12.1752 14.2677 11.9619Z"
+                                            fill="#F94743" />
+                                        <path
+                                            d="M15.2132 9.63705C14.8743 9.53077 14.5133 9.71946 14.4071 10.0584C14.3664 10.1879 14.3214 10.318 14.2728 10.4454C14.166 10.726 14.2683 11.0346 14.5028 11.2C14.5459 11.2302 14.5933 11.2559 14.6448 11.2753C14.9766 11.402 15.3482 11.2355 15.4747 10.9035C15.5325 10.752 15.5861 10.5972 15.6345 10.4432C15.7406 10.1042 15.5521 9.74333 15.2132 9.63705Z"
+                                            fill="#F94743" />
+                                        <path
+                                            d="M6.80993 14.6652C6.23486 14.5619 5.68322 14.3861 5.16012 14.1409C5.15393 14.1376 5.1484 14.1339 5.1419 14.131C5.01863 14.073 4.89557 14.0108 4.77629 13.9459C4.77588 13.9454 4.77512 13.9451 4.77447 13.9448C4.55562 13.8243 4.34203 13.691 4.13454 13.5449C1.10884 11.4135 0.381345 7.21787 2.51287 4.19221C2.97636 3.53453 3.53725 2.98585 4.16257 2.55097C4.17027 2.54561 4.17797 2.54028 4.18561 2.53488C6.38912 1.0166 9.38114 0.914276 11.7209 2.47445L11.2184 3.20054C11.0787 3.40263 11.1646 3.54991 11.4092 3.5279L13.5921 3.33247C13.837 3.31046 13.9834 3.09863 13.9175 2.86213L13.3314 0.750148C13.2657 0.513381 13.0977 0.485041 12.9579 0.687104L12.4542 1.41491C10.7371 0.262272 8.67765 -0.177246 6.63352 0.177216C6.42764 0.212848 6.22465 0.256459 6.02444 0.307225C6.02289 0.3075 6.02166 0.307672 6.02042 0.307947C6.01268 0.309839 6.00484 0.312349 5.9973 0.314447C4.23462 0.767104 2.6967 1.79517 1.5993 3.262C1.59005 3.27297 1.58052 3.2837 1.57178 3.29563C1.53529 3.34478 1.49908 3.39507 1.46362 3.44535C1.40563 3.52776 1.34847 3.61223 1.29378 3.6967C1.28693 3.70688 1.28171 3.71723 1.27572 3.72752C0.370133 5.13082 -0.0666682 6.75318 0.00824147 8.40483C0.00841344 8.41027 0.0081039 8.41574 0.00824147 8.42131C0.0154986 8.58265 0.0282931 8.74623 0.0456619 8.90726C0.0465906 8.91765 0.048895 8.92748 0.050649 8.93787C0.0686026 9.0998 0.0911306 9.2621 0.119368 9.42437C0.40635 11.08 1.18736 12.5699 2.35768 13.7291C2.3604 13.7318 2.36322 13.7348 2.36597 13.7376C2.36693 13.7387 2.368 13.7392 2.36893 13.7402C2.68336 14.0503 3.02537 14.3372 3.39362 14.5965C4.35734 15.2756 5.43046 15.7245 6.58293 15.9313C6.93261 15.9941 7.26664 15.7614 7.32938 15.4119C7.39208 15.0621 7.15951 14.7278 6.80993 14.6652Z"
+                                            fill="white" />
+                                        <path
+                                            d="M7.60534 2.86133C7.3177 2.86133 7.08472 3.09452 7.08472 3.38174V8.5665L11.8266 11.0178C11.9029 11.0573 11.9846 11.0759 12.065 11.0759C12.2534 11.0759 12.4353 10.9733 12.5278 10.7944C12.6597 10.539 12.5599 10.2252 12.3045 10.0933L8.12537 7.93276V3.38174C8.12534 3.09452 7.89263 2.86133 7.60534 2.86133Z"
+                                            fill="#F94743" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_70_902">
+                                            <rect width="16" height="16" fill="white" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                                <p class="text-secondary ms-3 fs-4 mb-0">Sabtu</p>
+                            </div>
+                            <span class="text-white fs-3">09.00-14.00 WIB
+                            </span>
+                        </div>
+
+                    </div>
+                    <div class="col-md-12">
                         <div class="pb-4">
                             <div class="d-flex align-items-center mb-3">
                                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
@@ -109,61 +164,64 @@
                                         d="M8 7.5C9.03553 7.5 9.875 6.66053 9.875 5.625C9.875 4.58947 9.03553 3.75 8 3.75C6.96447 3.75 6.125 4.58947 6.125 5.625C6.125 6.66053 6.96447 7.5 8 7.5Z"
                                         fill="#F94743" />
                                 </svg>
-                                <p class="text-secondary ms-3 fs-4 mb-0">Office Address 2</p>
+                                <p class="text-secondary ms-3 fs-4 mb-0">Alamat Kantor</p>
                             </div>
-                            <span class="text-white ps-4 fs-3">195 Devonshire St</span>
+                            <span class="text-white fs-3">Jl. Ringroad Selatan no.6, Glugo, Panggungharjo, Kec.
+                                Sewon, Kab. Bantul, Daerah Istimewa Yogyakarta, 55188
+                            </span>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-7 ps-lg-10 pt-5">
-                <div class="row wow img-custom-anim-top">
-                    <div class="col-md-6 pe-md-4">
-                        <div class="input-group mb-5">
-                            <input type="text"
-                                class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
-                                placeholder="Your name" aria-label="Recipient's username" />
-                            <i
-                                class="bi bi-person text-white d-flex border-bottom border-secondary  align-items-center"></i>
+            <div class="col-lg-6 ps-lg-10 pt-5">
+                <form action="" method="">
+                    <div class="row wow img-custom-anim-top">
+                        <div class="col-md-6 pe-md-4">
+                            <div class="input-group mb-5">
+                                <input type="text"
+                                    class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
+                                    placeholder="Nama anda" aria-label="Recipient's username" />
+                                <i
+                                    class="bi bi-person text-white d-flex border-bottom border-secondary  align-items-center"></i>
+                            </div>
+                            <div class="input-group mb-5 mt-4">
+                                <input type="text"
+                                    class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
+                                    placeholder="Whatsapp" aria-label="Recipient's username" />
+                                <i
+                                    class="bi bi-telephone text-white d-flex border-bottom border-secondary  align-items-center"></i>
+                            </div>
                         </div>
-                        <div class="input-group mb-5 mt-4">
-                            <input type="text"
-                                class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
-                                placeholder="Phone number" aria-label="Recipient's username" />
-                            <i
-                                class="bi bi-telephone text-white d-flex border-bottom border-secondary  align-items-center"></i>
+                        <div class="col-md-6 ps-md-4">
+                            <div class="input-group mb-5">
+                                <input type="text"
+                                    class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
+                                    placeholder="info@meravi.id" aria-label="Username" />
+                                <i
+                                    class="bi bi-envelope-open text-white d-flex border-bottom border-secondary  align-items-center"></i>
+                            </div>
+                            <div class="input-group mb-5">
+                                <input type="text"
+                                    class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
+                                    placeholder="Subjek" aria-label="Subject" />
+                                <i
+                                    class="bi bi-file-earmark text-white d-flex border-bottom border-secondary  align-items-center"></i>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="input-group">
+                                <textarea class="form-control bg-black border-0 border-bottom border-secondary rounded-0 pb-10"
+                                    placeholder="Masukan pesan anda" aria-label="With textarea"></textarea>
+                                <i
+                                    class="bi bi-pencil text-white d-flex border-bottom border-secondary align-items-start mt-2"></i>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6 ps-md-4">
-                        <div class="input-group mb-5">
-                            <input type="text"
-                                class="form-control bg-black border-0 border-bottom border-secondary rounded-0"
-                                placeholder="info@abon.com" aria-label="Username" />
-                            <i
-                                class="bi bi-envelope-open text-white d-flex border-bottom border-secondary  align-items-center"></i>
-                        </div>
-                        <select
-                            class="form-select text-secondary bg-transparent border-0 border-bottom border-secondary rounded-0"
-                            aria-label="Default select example">
-                            <option class="bg-black" value="receiving-department" selected>Receiving department</option>
-                            <option class="bg-black" value="technical-assistance">Technical assistance</option>
-                            <option class="bg-black" value="business-support">Business Support</option>
-                            <option class="bg-black" value="agent-support">Agent support</option>
-                        </select>
+                    <div class="mt-5">
+                        <button type="submit" class="btn-gradient mt-2">Save & Submit<i
+                                class="bi bi-arrow-right ms-2"></i></button>
                     </div>
-                    <div class="col-12">
-                        <div class="input-group">
-                            <textarea class="form-control bg-black border-0 border-bottom border-secondary rounded-0 pb-10"
-                                placeholder="Describe Your Project in Short" aria-label="With textarea"></textarea>
-                            <i
-                                class="bi bi-pencil text-white d-flex border-bottom border-secondary align-items-start mt-2"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-5">
-                    <a href="#demo" class="btn-gradient mt-2">Save & Submit<i
-                            class="bi bi-arrow-right ms-2"></i></a>
-                </div>
+                </form>
             </div>
         </div>
     </div>
