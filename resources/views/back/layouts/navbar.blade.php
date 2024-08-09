4c0473868462bdbf95b257873fdb16f46b46dd45
nyvansyn <syncore.nyvan@gmail.com>
2024-08-09 16:00:02 +0700

    update navbar dashboard admin

--- resources/views/back/layouts/navbar.blade.php
+++ resources/views/back/layouts/navbar.blade.php
@@ -6,17 +6,16 @@
         <div class="be-right-navbar">
             <ul class="nav navbar-nav float-right be-user-nav">
                 <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
-                        role="button" aria-expanded="false"><img src="assets/img/avatar.png" alt="Avatar"><span
-                            class="user-name">Túpac
-                            Amaru</span></a>
+                        role="button" aria-expanded="false"><img src="{{ asset('back/assets/img/avatar.png') }}"
+                            alt="Avatar"><span class="user-name">{{ Auth::user()->name }}</span></a>
                     <div class="dropdown-menu" role="menu">
                         <div class="user-info">
-                            <div class="user-name">Túpac Amaru</div>
-                            <div class="user-position online">Available</div>
-                        </div><a class="dropdown-item" href="pages-profile.html"><span
-                                class="icon mdi mdi-face"></span>Account</a><a class="dropdown-item"
-                            href="#"><span class="icon mdi mdi-settings"></span>Settings</a><a
-                            class="dropdown-item" href="pages-login.html"><span
+                            <div class="user-name">{{ Auth::user()->name }}</div>
+                        </div>
+                        {{-- <a class="dropdown-item" href="pages-profile.html"><span
+                                class="icon mdi mdi-face"></span>Account</a>
+                        <a class="dropdown-item" href="#"><span class="icon mdi mdi-settings"></span>Settings</a> --}}
+                        <a class="dropdown-item" href="{{ route('logout') }}"><span
                                 class="icon mdi mdi-power"></span>Logout</a>
                     </div>
                 </li>
