6de28696212fa158196f22eef99769dd72abe443
nyvansyn <syncore.nyvan@gmail.com>
2024-09-04 10:53:20 +0700

    add floating wa

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,6 +1,25 @@
 <!-- Footer -->
+
 <footer>
     <div class="container-fluid footer-1 px-0">
+        <a href="https://wa.me/6287805900800" target="_blank" class="whatsapp-btn"
+            style="background: rgba(13, 174, 24, 1);
+  position: fixed;
+  right: 30px;
+  bottom: 80px;
+  height: 50px;
+  width: 50px;
+  cursor: pointer;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  border-radius: 50px;
+  z-index: 99;
+  color: white;
+  font-size: 30px;
+  font-weight: bold;">
+            <i class="bi bi-whatsapp"></i>
+        </a>
         <div class="footer">
             <div class="container py-4">
                 <div class="row text-secondary">
@@ -11,18 +30,24 @@
                     </div>
                     <div class="col d-flex justify-content-end">
                         <div class="d-flex gap-4">
-                            {{-- <a target="blank" href="https://www.facebook.com/meravi.id" class="text-reset">
-                                <i class="bi bi-facebook"></i>
-                            </a>
-                            <a target="blank" href="https://twitter.com/" class="text-reset">
-                                <i class="bi bi-twitter-x"></i>
-                            </a> --}}
+                            <!--<a target="blank" href="https://www.facebook.com/meravi.id" class="text-reset">-->
+                            <!--    <i class="bi bi-facebook"></i>-->
+                            <!--</a>-->
+                            <!--<a target="blank" href="https://twitter.com/" class="text-reset">-->
+                            <!--    <i class="bi bi-twitter-x"></i>-->
+                            <!--</a>-->
+                            <!--<a target="blank" href="https://wa.me/6287805900800" class="text-resets whatsapp-btn">-->
+                            <!--    <i class="bi bi-whatsapp"></i>-->
+                            <!--</a>-->
                             <a target="blank" href="https://www.instagram.com/meravi.id?igsh=MXhtNDc3aWJ1ZG1mbA=="
                                 class="text-reset">
                                 <i class="bi bi-instagram"></i>
                             </a>
                             <a target="blank" href="https://www.youtube.com/@MeraviTV/" class="text-reset">
                                 <i class="bi bi-youtube"></i>
+                            </a>
+                            <a target="blank" href="https://www.instagram.com/bumdes.id/" class="text-reset">
+                                <i class="bi bi-instagram"></i>
                             </a>
                         </div>
                     </div>
